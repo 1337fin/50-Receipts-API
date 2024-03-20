0378194b585db6e28a1a2b639119740c44681dd9
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-20 02:11:31 +0000

    Update apple.py

--- examples/apple.py
+++ examples/apple.py
@@ -1,29 +1,12 @@
 import requests
 
-url = "http://api.50receiptscom/api/apple"
+url = "http://api.50receipts.com/api/apple"
 
 data = {
     "discordId": "YOUR DISCORD ID",
     "apiKey": "YOUR API KEY",
-    
-    "product_link": "YOUR PRODUCT LINK",
-    "email": "test@example.com",
-    "size": "YOUR SIZE",
-    "price": 100.00,
-    "shipping_fee": 10.00,
-    "tax_fee": 1.00,
-    "currency": "£",
-    "currency_tag": "GBP",
-    "full_name": "50 Receipts",
-    "address": "YOUR ADDRESS",
-    "city": "YOUR CITY",
-    "postal_code": "YOUR POSTAL CODE",
-    "country": "YOUR COUNTRY",
-    "receipt_type": 3,
-    "order_date": "01/01/2024",
-    "estimated_date": "07/01/2024",
-    
 
+    # All data additions will be shown to you when you run the file.
 }
 
 r = requests.post(url, data=data)
