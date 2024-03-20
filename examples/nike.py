c6438282c4829c35413786de65d48e3958c21f01
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-20 02:00:41 +0000

    Update nike.py

--- examples/nike.py
+++ examples/nike.py
@@ -7,7 +7,7 @@ data = {
     "discordId": "YOUR DISCORD ID",
     "apiKey": "YOUR API KEY",
     "product_link": prl,
-    "email": "YOUR EMAIL",
+    "email": "test@example.com",
     "size": "YOUR SIZE",
     "price": 100.00,
     "shipping_fee": 10.00,
@@ -22,9 +22,11 @@ data = {
     "receipt_type": 3, # 1 = Nike.com | 2 = Nike SNKRS | 3 = Nike You Got 'Em
     "order_date": "01/01/2024",
     "estimated_date": "07/01/2024",
-    
-
 }
+
+# Receipt Type 1 = Nike.com
+# Receipt Type 2 = Nike SNKRS
+# Receipt Type 3 = Nike You Got 'Em
 
 r = requests.post(url, data=data)
 print(r.text)
