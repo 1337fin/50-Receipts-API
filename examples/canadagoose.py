ec231d17d9556680bd418ff11dd6f3ff1414f48b
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-20 02:15:23 +0000

    Create canadagoose.py

---
+++ examples/canadagoose.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://api.50receipts.com/api/canadagoose"
+
+data = {
+    "discordId": "YOUR DISCORD ID",
+    "apiKey": "YOUR API KEY",
+
+    # All data additions will be shown to you when you run the file.
+}
+
+r = requests.post(url, data=data)
+print(r.text)
