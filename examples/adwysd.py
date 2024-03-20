139624fae685112fd71ba35f95aaa703017710d9
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-20 02:11:17 +0000

    Update adwysd.py

--- examples/adwysd.py
+++ examples/adwysd.py
@@ -5,20 +5,8 @@ url = "http://api.50receipts.com/api/adwysd"
 data = {
     "discordId": "YOUR DISCORD ID",
     "apiKey": "YOUR API KEY",
-  
-    "product_link": "YOUR PRODUCT LINK",
-    "email": "test@example.com",
-    "size": "YOUR SIZE",
-    "price": 100.00,
-    "shipping_fee": 10.00,
-    "tax_fee": 1.00,
-    "currency": "£",
-    "currency_tag": "GBP",
-    "full_name": "50 Receipts",
-    "address": "YOUR ADDRESS",
-    "city": "YOUR CITY",
-    "postal_code": "YOUR POSTAL CODE",
-    "country": "YOUR COUNTRY",
+
+    # All data additions will be shown to you when you run the file.
 }
 
 r = requests.post(url, data=data)
