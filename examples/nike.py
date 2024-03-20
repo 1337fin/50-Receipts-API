a798fef16783424fd0be82cb4a3c275df1eaf3ac
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-20 02:11:58 +0000

    Update nike.py

--- examples/nike.py
+++ examples/nike.py
@@ -1,33 +1,17 @@
 import requests
 
-url = "http://api.50receipts.com/api/nike"
-
+url = "http://api.50receipts.com/api/adwysd"
 
 data = {
     "discordId": "YOUR DISCORD ID",
     "apiKey": "YOUR API KEY",
-    
-    "product_link": "YOUR PRODUCT LINK",
-    "email": "test@example.com",
-    "size": "YOUR SIZE",
-    "price": 100.00,
-    "shipping_fee": 10.00,
-    "tax_fee": 1.00,
-    "currency": "£",
-    "currency_tag": "GBP",
-    "full_name": "50 Receipts",
-    "address": "YOUR ADDRESS",
-    "city": "YOUR CITY",
-    "postal_code": "YOUR POSTAL CODE",
-    "country": "YOUR COUNTRY",
-    "receipt_type": 3, # 1 = Nike.com | 2 = Nike SNKRS | 3 = Nike You Got 'Em
-    "order_date": "01/01/2024",
-    "estimated_date": "07/01/2024",
+
+    # All data additions will be shown to you when you run the file.
 }
+
+r = requests.post(url, data=data)
+print(r.text)
 
 # Receipt Type 1 = Nike.com
 # Receipt Type 2 = Nike SNKRS
 # Receipt Type 3 = Nike You Got 'Em
-
-r = requests.post(url, data=data)
-print(r.text)
