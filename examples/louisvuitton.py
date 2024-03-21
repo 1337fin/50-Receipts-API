87d9c43f258c95e9c318183ba18bdef0d2530d58
fin702106 <119663332+fin702106@users.noreply.github.com>
2024-03-21 21:53:16 +0000

    Update louisvuitton.py

--- examples/louisvuitton.py
+++ examples/louisvuitton.py
@@ -5,6 +5,8 @@ url = "http://api.50receipts.com/api/louisvuitton"
 data = {
     "discordId": "YOUR DISCORD ID",
     "apiKey": "YOUR API KEY",
+    "gender": "male", # male or female
+    "size": "nothing" # type nothing in size if there isn't a size
 
     # All data additions will be shown to you when you run the file.
 }
